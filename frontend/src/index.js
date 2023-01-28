62635b72893545aad893b9004a6be83dd327739e
Vladyslav Radetskyi <109795929+Radyslav818@users.noreply.github.com>
2023-01-28 18:03:24 +0200

    get api, create store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsReducer, { productsFetch } from "./features/productsSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+store.dispatch(productsFetch());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
