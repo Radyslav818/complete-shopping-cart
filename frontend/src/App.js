c8fb9af64c233d45f1e1e6e9f26a3fcdf40189c1
Vladyslav Radetskyi <109795929+Radyslav818@users.noreply.github.com>
2023-01-28 13:11:50 +0200

    create componets and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import NotFound from "./components/NotFound";
-import Cart from "./components/Cart";
-
+import NavBar from "./componets/NavBar";
+import Cart from "./componets/Cart";
+import Home from "./componets/Home";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <ToastContainer />
         <NavBar />
-        <div className="content-container">
-          <Switch>
-            <Route path="/cart" component={Cart} />
-            <Route path="/not-found" component={NotFound} />
-            <Route path="/" exact component={Home} />
-            <Redirect to="/not-found" />
-          </Switch>
-        </div>
+        <Switch>
+          <Route path="/cart" component={Cart} />
+          <Route path="/" component={Home} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
